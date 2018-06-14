7ee4631e256b00252bcc01c8b2c72d8559ffecdb
Mykhailo Pylypenko <musicvyp@gmail.com>
2018-06-14 10:55:37 +0300

    First block - made it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import "./styles/styles";
-import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplaySpeed: 1200,
+        margin: 10,
+        nav: true,
+        dots: true,
+        items: 1,
+        navText: ["<img class='owl-nav-buttons_img' src='../assets/images/arrow.png'>", "<img class='owl-nav-buttons_img' src='../assets/images/arrow.png'>"]
+    })
+})
